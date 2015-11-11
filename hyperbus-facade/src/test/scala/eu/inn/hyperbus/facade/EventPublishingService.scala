b1dee13e1367da4b790a403a2fdc31dcc38b76b8
alexey olenev <a.a.olenev@gmail.com>
2015-11-11 13:17:41 +0300

    Event publishing service and tests for it

--- hyperbus-facade/src/test/scala/eu/inn/hyperbus/facade/EventPublishingService.scala
+++ hyperbus-facade/src/test/scala/eu/inn/hyperbus/facade/EventPublishingService.scala
@@ -3,20 +3,20 @@ package eu.inn.hyperbus.facade
 import java.util.concurrent.atomic.AtomicInteger
 
 import com.typesafe.config.ConfigFactory
-import eu.inn.binders.dynamic.{Obj, Text}
 import eu.inn.hyperbus.HyperBus
 import eu.inn.hyperbus.model._
-import eu.inn.hyperbus.model.annotations.body
+import eu.inn.hyperbus.model.annotations.{body, request}
 import eu.inn.hyperbus.model.standard._
-import eu.inn.hyperbus.transport.api.{PublishResult, TransportConfigurationLoader, TransportManager}
+import eu.inn.hyperbus.transport.api._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 @body("application/vnd+test-1.json")
-case class TestBodyForFacade(content: Obj) extends Body
+case class TestBodyForFacade(content: String) extends Body
 
-case class TestRequestForFacade(url: String, body: TestBodyForFacade, messageId: String, correlationId: String) extends StaticGet(body)
+@request("/test-facade/{content}")
+case class TestRequestForFacade(body: TestBodyForFacade, messageId: String, correlationId: String) extends StaticGet(body)
 
 class EventPublishingService {
   val requestCounter = new AtomicInteger
@@ -26,33 +26,34 @@ class EventPublishingService {
   val transportManager = new TransportManager(transportConfiguration)
   val hyperBus = new HyperBus(transportManager, Some("group1"))
   var subscriptionId: String = null
+  val defaultCallback = {() => println("default")}
 
   def publish (request: TestRequestForFacade): Future[PublishResult] = {
     unsubscribe
     hyperBus <| request
   }
 
-  def subscribeAndPublish(url: String = "/test-response{testPartitionId}", messageId: String = "123", correlationId: String = "456")
-                         (optionalTestCallback: (_ => _) = {})= {
+  def subscribeAndPublishDefaultResponseOnReceived(messageId: String = "123", correlationId: String = "456", optionalTestCallback: (() => _) = defaultCallback)= {
     unsubscribe
     subscriptionId = hyperBus |> { request: TestRequestForFacade =>
+      unsubscribe
       Future {
-        hyperBus <| TestRequestForFacade(url,
-          TestBodyForFacade(Obj(Map("resourceData" → Text(s"Test Event #${requestCounter.incrementAndGet()}")))),
+        optionalTestCallback()
+        hyperBus <| TestRequestForFacade(
+          TestBodyForFacade("ha ha"),
           messageId,
           correlationId)
-        optionalTestCallback
       }
     }
   }
 
-  def subscribeAndPublish(requestToReplyWith: TestRequestForFacade)
-                         (optionalTestCallback: (_ => _) = {}): Unit = {
+  def subscribeAndPublishOnReceived(requestToReplyWith: TestRequestForFacade, optionalTestCallback: (() => _) = defaultCallback): Unit = {
     unsubscribe
     subscriptionId = hyperBus |> { request: TestRequestForFacade =>
+      unsubscribe
       Future {
+        optionalTestCallback()
         hyperBus <| requestToReplyWith
-        optionalTestCallback
       }
     }
   }
--- hyperbus-facade/src/test/scala/eu/inn/hyperbus/facade/EventPublishingServiceTest.scala
+++ hyperbus-facade/src/test/scala/eu/inn/hyperbus/facade/EventPublishingServiceTest.scala
@@ -1,7 +1,6 @@
 package eu.inn.hyperbus.facade
 
 import com.typesafe.config.ConfigFactory
-import eu.inn.binders.dynamic.{Obj, Text}
 import eu.inn.hyperbus.HyperBus
 import eu.inn.hyperbus.transport.api.{TransportConfigurationLoader, TransportManager}
 import org.scalatest.concurrent.PatienceConfiguration.Timeout
@@ -9,8 +8,10 @@ import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Seconds, Span}
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, FreeSpec, Matchers}
 
-import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Promise, Await, Future}
+import scala.util.Success
 
 /**
  * This tests requires Kafka up and running
@@ -19,12 +20,12 @@ class EventPublishingServiceTest extends FreeSpec with ScalaFutures with Matcher
   val transportConfiguration = TransportConfigurationLoader.fromConfig(ConfigFactory.load())
   val transportManager = new TransportManager(transportConfiguration)
   val publishingService = new EventPublishingService
-  val hyperBus = new HyperBus(transportManager, Some("group1"))
+  var hyperBus = new HyperBus(transportManager, Some("group1"))
   
   after {
     publishingService.unsubscribe
   }
-  
+
   override def afterAll(): Unit = {
     val timeout = 10.seconds
     try {
@@ -36,44 +37,69 @@ class EventPublishingServiceTest extends FreeSpec with ScalaFutures with Matcher
   
   "EventPublishingService " - {
     "publish" in {
-      val request = TestRequestForFacade("/test-request{testPartitionId}",
-        TestBodyForFacade(Obj(Map("resourceData" → Text("Test Event")))),
+      val request = TestRequestForFacade(TestBodyForFacade("ha ha"),
         "123",
         "456")
       val receiveFuture = Future {}
-      hyperBus |> {request: TestRequestForFacade =>
+      val subscriptionId = hyperBus |> { request: TestRequestForFacade =>
         receiveFuture
       }
-      
+
       publishingService.publish(request)
       whenReady(receiveFuture, Timeout(Span(5, Seconds))) { _ => }
+      hyperBus.off(subscriptionId)
     }
 
-    "subscribeAndPublish" in {
-      val initialRequest = TestRequestForFacade("/test-request{testPartitionId}",
-        TestBodyForFacade(Obj(Map("resourceData" → Text("Test Event")))),
+    "subscribeAndPublishOnReceived" in {
+      val initialRequest = TestRequestForFacade(TestBodyForFacade("ha ha"),
         "requestMessage",
         "requestCorrelationId")
-      val requestToReplyWith = TestRequestForFacade("/test-request{testPartitionId}",
-        TestBodyForFacade(Obj(Map("resourceData" → Text("Test Event")))),
+      val requestToReplyWith = TestRequestForFacade(TestBodyForFacade("ha ha"),
         "responseMessage",
         "responseCorrelationId")
       var echoRequest: TestRequestForFacade = null
-      var onEchoReceivedFuture: Future[Unit] = null
-      val onReceive: (Unit => String) = {() =>
-        hyperBus |> {request: TestRequestForFacade =>
-          onEchoReceivedFuture = Future {
-            echoRequest = request
-          }
-          onEchoReceivedFuture
+      val onEchoReceived: Promise[Unit] = Promise()
+      var subscriptionId: String = null
+      val onReceive = { () =>
+        subscriptionId = hyperBus |> { request: TestRequestForFacade =>
+          onEchoReceived.success({echoRequest = request})
+          onEchoReceived.future
         }
       }
 
-      publishingService.subscribeAndPublish(requestToReplyWith) (onReceive)
-      hyperBus <| initialRequest
-      whenReady(onEchoReceivedFuture, Timeout(Span(5, Seconds))) { _ =>
-        echoRequest should equal(requestToReplyWith)
+      publishingService.subscribeAndPublishOnReceived(requestToReplyWith, onReceive)
+      whenReady(hyperBus <| initialRequest, Timeout(Span(5, Seconds))) { _ =>
+        whenReady(onEchoReceived.future, Timeout(Span(5, Seconds))) { _ =>
+          echoRequest should equal(requestToReplyWith)
+        }
       }
+      hyperBus.off(subscriptionId)
+    }
+
+    "subscribeAndPublishDefaultResponseOnReceived" in {
+      val initialRequest = TestRequestForFacade(TestBodyForFacade("ha ha"),
+        "requestMessage",
+        "requestCorrelationId")
+      val expectedEchoRequest = TestRequestForFacade(TestBodyForFacade("ha ha"),
+        "responseMessage",
+        "responseCorrelationId")
+      var echoRequest: TestRequestForFacade = null
+      val onEchoReceived: Promise[Unit] = Promise()
+      var subscriptionId: String = null
+      val onReceive = { () =>
+        subscriptionId = hyperBus |> { request: TestRequestForFacade =>
+          onEchoReceived.success({echoRequest = request})
+          onEchoReceived.future
+        }
+      }
+
+      publishingService.subscribeAndPublishDefaultResponseOnReceived("responseMessage", "responseCorrelationId", onReceive)
+      whenReady(hyperBus <| initialRequest, Timeout(Span(5, Seconds))) { _ =>
+        whenReady(onEchoReceived.future, Timeout(Span(5, Seconds))) { _ =>
+          echoRequest should equal(expectedEchoRequest)
+        }
+      }
+      hyperBus.off(subscriptionId)
     }
   }
 }
