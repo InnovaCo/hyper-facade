16ccbf93547320b6e82697cdb3a16ac265c1d49b
alexey olenev <a.a.olenev@gmail.com>
2015-12-08 10:02:21 +0300

    Remove unused classes

--- src/main/scala/eu/inn/facade/ComponentRegistry.scala
+++ src/main/scala/eu/inn/facade/ComponentRegistry.scala
@@ -2,7 +2,6 @@ package eu.inn.facade
 
 import eu.inn.facade.events.SubscriptionsManager
 import eu.inn.facade.filter.chain.FilterChainStubComponent
-import eu.inn.facade.filter.{FilteringDirective, RequestFilterComponent}
 import eu.inn.facade.http.{WsRestServiceComponent, StatusMonitorFacade}
 import eu.inn.util.{ConsoleIOComponent, ConfigComponent, Logging}
 import eu.inn.util.akka.ActorSystemComponent
