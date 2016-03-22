213d1559cd3f43e8eeef81c91dd91753fb15cfdc
Maga Abdurakhmanov <maqdev@gmail.com>
2016-03-22 18:09:00 +0100

    + working on response links transformation (incomplete)

--- src/main/scala/eu/inn/facade/filter/http/HttpWsResponseFilter.scala
+++ src/main/scala/eu/inn/facade/filter/http/HttpWsResponseFilter.scala
@@ -1,8 +1,11 @@
 package eu.inn.facade.filter.http
 
+import eu.inn.binders.dynamic.{Lst, Obj, ObjV, Value}
 import eu.inn.facade.model._
 import eu.inn.facade.utils.NamingUtils
 import eu.inn.hyperbus.model.Header
+import eu.inn.hyperbus.transport.api.matchers.{Specific, TextMatcher}
+import eu.inn.hyperbus.transport.api.uri.{Uri, UriParser}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -24,12 +27,67 @@ class HttpWsResponseFilter extends ResponseFilter {
           }
       }
 
+      val newBody = if (response.body.isInstanceOf[Obj]) {
+        Obj(response.body.asMap.map {
+          case ("_links", value) ⇒
+            "_links" → {
+              value match {
+                case Obj(links) ⇒ Obj(transformLinks(links, response.body))
+                case other ⇒ other
+              }
+            }
+
+          case ("_embedded", value) ⇒
+            "_embedded" → {
+              value match {
+                case Obj(embedded) ⇒ Obj(transformEmbedded(embedded))
+                case other ⇒ other
+              }
+            }
+          case (key, value) ⇒
+            key → value
+        })
+      } else {
+        response.body
+      }
+
       response.copy(
-        headers = headersBuilder.result()
+        headers = headersBuilder.result(),
+        body = newBody
       )
     }
   }
+
+  def transformLink(linkValue: Value, body: Value): Value = {
+    if (linkValue.templated[Option[Boolean]].contains(true)) { // templated link, have to format
+    val href = linkValue.href[String]
+      val tokens = UriParser.extractParameters(href)
+      val args = tokens.map { arg ⇒
+        arg → linkValue.selectDynamic[String](arg)             // todo: support inner fields
+      } toMap
+      val uri = Uri(href, args)
+      ObjV("href" → uri.formatted)
+    } else {
+      linkValue
+    }
+  }
+
+  def transformLinks(links: scala.collection.Map[String, Value], body: Value) : scala.collection.Map[String, Value] = {
+    links map {
+      case (name, links : Lst) ⇒ // json+hal when link is array
+        name → Lst(links.v.map(transformLink(_, body)))
+      case (name, link) ⇒ // json+hal - single link
+        name → transformLink(link, body)
+    }
+  }
+
+  def transformEmbedded(embedded: scala.collection.Map[String, Value]) : scala.collection.Map[String, Value] = {
+    embedded map { case (name, value) ⇒
+
+    }
+  }
 }
+
 
 object HttpWsResponseFilter {
   val directHyperbusToFacade = FacadeHeaders.directHeaderMapping.map(kv ⇒ kv._2 → kv._1).toMap
