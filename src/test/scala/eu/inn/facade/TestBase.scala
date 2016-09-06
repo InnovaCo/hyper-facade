cf865392d077989947f64a25885b04a59f7d194a
alexey olenev <aleksey.olenev@inn.ru>
2016-09-06 19:47:58 +0300

    play with timeouts

--- src/test/scala/eu/inn/facade/TestBase.scala
+++ src/test/scala/eu/inn/facade/TestBase.scala
@@ -1,14 +1,16 @@
 package eu.inn.facade
 
+import eu.inn.facade.raml.RewriteIndexHolder
 import eu.inn.facade.workers.TestWsRestServiceApp
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, FreeSpec, Matchers}
 import scaldi.Injectable
 
-abstract class TestBase extends FreeSpec with Matchers with ScalaFutures with CleanRewriteIndex with Injectable with MockContext with BeforeAndAfterAll {
+abstract class TestBase extends FreeSpec with Matchers with ScalaFutures with Injectable with MockContext with BeforeAndAfterAll {
 
   def app: TestWsRestServiceApp
   override def afterAll(): Unit = {
+    RewriteIndexHolder.clearIndex()
     app.shutdown()
   }
 }
--- src/test/scala/eu/inn/facade/integration/IntegrationTestBase.scala
+++ src/test/scala/eu/inn/facade/integration/IntegrationTestBase.scala
@@ -23,7 +23,7 @@ class IntegrationTestBase(val ramlFilePath: String) extends TestBase
 
   implicit val injector = TestInjectors()
   implicit val actorSystem = inject[ActorSystem]
-  implicit val patience = PatienceConfig(scaled(Span(15, Seconds)))
+  implicit val patience = PatienceConfig(scaled(Span(10, Seconds)))
   implicit val timeout = akka.util.Timeout(15.seconds)
   implicit val uid = new UriSpecificDeserializer
   implicit val uis = new UriSpecificSerializer
